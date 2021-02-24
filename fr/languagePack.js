cf4417d25848b32be3ad4ec2a1bac8f95aa9e1b7
Mysterken <Mysterkenzo@gmail.com>
2021-02-24 18:38:36 +0100

    French language pack

---
+++ fr/languagePack.js
@@ -0,0 +1,78 @@
+/* ===========================================================
+ * languagePack.js
+ * ===========================================================
+ * Copyright 2021 @Acivev
+ * Licensed under Apache 2.0, January 2004
+ * ========================================================== */
+export const languagePack = {
+    navbar:{
+        "about":["À propos"],
+        "leaderboard":["Classement"],
+        "community":["Communauté"],
+        "faq":["FAQ"],
+        "home":["Accueil"],
+        "loging":["S'identifier/S'enregistrer"],
+        "language":["#flag#"]
+    },
+
+    splash:{
+        "text":["osu!droid"],
+        "update":["Dernière Version: 1.6.7 | dernière mise à jour: "],
+        "preupdate":["Version Pré-Publication: 1.6.8 | première phase"],
+        "download":["Télécharger!"],
+        "github":["GitHub!"]
+    },
+
+    alert:{
+        "announcment":["Annonce"],
+        "text":["Nous n'avons jamais publié notre jeu au Play Store. Si jamais vous le voyez là-bas, veuillez ne pas télécharger ou acheter l'application!"]
+    },
+    about:{
+        "about":["à propos"],
+        "text":["osu!droid est un jeu de rythme pour smartphones android. C'était un jeu né il y a plusieurs années dans la communauté de joueurs, osu! Il est maintenant développé par un groupe de fans et avec l'aide d'ami étranger. Il s'agit aussi d'un jeu complètement gratuit à jouer."],
+        "text1":["Le programme de ce jeu et ce site web ne sont aucunement affiliés à osu! ou ppy."]
+    },
+
+    leaderboard:{
+        "leaderboard":["Classsement"],
+        "rank":["#"],
+        "playername":["Nom du joueur"],
+        "score":["Score"],
+        "accuracy":["Précision"],
+        "region":["Région"],
+        "text":["Chaque Beatmap a un classement vous permettant de rivaliser avec chaque joueur du jeu! Voici une liste du top 100 des joueurs mondiaux."]
+    },
+
+    community:{
+        "community":["Rejoignez notre communauté florissante!"],
+        "qq":["Groupe QQ","groupid: 577344771"],
+        "baidu":["Forum Baidu","Chinois uniquement."],
+        "discord":["Discord", "Rejoignez notre serveur Discord pour discuter avec des centaines d'autres joueurs osu! Droid!"],
+        "bilibili":["Compte officiel Bilibili"]
+    },
+
+    faq:{
+        "faq":["Foire aux questions (FAQ)"],
+        "gameQuestion":["Je n'ai jamais joué à ce jeu auparavant", "par où débuter?"],
+            "gameAnswer":["Nous vous recommandons de débuter avec", "cet article"],
+        "beatmapsQuestion":["Comment obtenir plus de Beatmaps?"],
+            "beatmampsAnswer":["Vous pouvez en télécharger depuis", "le site osu! officiel", ", à travers des sites miroir de beatmaps osu! tel que", "Chimue(Bloodcat),","sayobot","etc.","ou en utilisant un téléchargeur de Beatmaps sur Android tel que","BeatmapService"],
+        "deviceQuestion":["Quelle est la configuration minimale pour jouer au jeu?"],
+            "deviceAnswer":["Utilisez n'importe quel appareil Android ayant une version égale ou supérieure à 2.3 pour jouer.", "Nous savons que certains téléphone de la marque Samsung ont des problèmes de compatibilité. Il est recommandé d'essayer une ROM tierce."],
+        "moreFaq":["Cliquez", "ici", "pour plus de questions."]
+    },
+
+    safety:{
+        "safety":["Sécurité"],
+        "text":["Ne jouez pas durant de trop longue session! Souvenez-vous à prendre des pauses!", "Vos mains ne sont pas pour osu!, mais pour votre vie."]
+    },
+
+    footer:{
+        "contact":["Contact"],
+        "report":["Signaler un abus"],
+        "email":["report@dgsrz.com | webmaster@acivev.com"],
+        "others":["Autres"],
+        "email2":["webmaster@dgsrz.com | webmaster@acivev.com"],
+        "copyright":["Copyright © 2015-2020, osu!droid Team ♥ and Page design by Geticer"]
+    }
+} 
